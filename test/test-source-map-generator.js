d5b400f45f2b659634fdb112bb6bdc1859ac8863
Nick Fitzgerald <fitzgen@gmail.com>
2011-07-16 02:22:09 -0700

    Adding a test for mapping between the SourceMapGenerator and the test map

--- test/test-source-map-generator.js
+++ test/test-source-map-generator.js
@@ -38,6 +38,7 @@ define(function (require, exports, module) {
 
   var assert = require('assert');
   var SourceMapGenerator = require('source-map/source-map-generator').SourceMapGenerator;
+  var testUtil = require('./util');
 
   exports['test some simple stuff'] = function () {
     var map = new SourceMapGenerator({
@@ -109,6 +110,94 @@ define(function (require, exports, module) {
         original: { line: 1, column: 1 }
       });
     });
+  };
+
+  exports['test that the correct mappings are being generated'] = function () {
+    var map = new SourceMapGenerator({
+      file: 'min.js',
+      sourceRoot: '/the/root'
+    });
+
+    map.addMapping({
+      generated: { line: 1, column: 1 },
+      original: { line: 1, column: 1 },
+      source: 'one.js'
+    });
+    map.addMapping({
+      generated: { line: 1, column: 5 },
+      original: { line: 1, column: 5 },
+      source: 'one.js'
+    });
+    map.addMapping({
+      generated: { line: 1, column: 9 },
+      original: { line: 1, column: 11 },
+      source: 'one.js'
+    });
+    map.addMapping({
+      generated: { line: 1, column: 18 },
+      original: { line: 1, column: 21 },
+      source: 'one.js',
+      name: 'bar'
+    });
+    map.addMapping({
+      generated: { line: 1, column: 21 },
+      original: { line: 2, column: 3 },
+      source: 'one.js'
+    });
+    map.addMapping({
+      generated: { line: 1, column: 28 },
+      original: { line: 2, column: 10 },
+      source: 'one.js',
+      name: 'baz'
+    });
+    map.addMapping({
+      generated: { line: 1, column: 32 },
+      original: { line: 2, column: 14 },
+      source: 'one.js',
+      name: 'bar'
+    });
+
+    map.addMapping({
+      generated: { line: 2, column: 1 },
+      original: { line: 1, column: 1 },
+      source: 'two.js'
+    });
+    map.addMapping({
+      generated: { line: 2, column: 5 },
+      original: { line: 1, column: 5 },
+      source: 'two.js'
+    });
+    map.addMapping({
+      generated: { line: 2, column: 9 },
+      original: { line: 1, column: 11 },
+      source: 'two.js'
+    });
+    map.addMapping({
+      generated: { line: 2, column: 18 },
+      original: { line: 1, column: 21 },
+      source: 'two.js',
+      name: 'n'
+    });
+    map.addMapping({
+      generated: { line: 2, column: 21 },
+      original: { line: 2, column: 3 },
+      source: 'two.js'
+    });
+    map.addMapping({
+      generated: { line: 2, column: 28 },
+      original: { line: 2, column: 10 },
+      source: 'two.js',
+      name: 'n'
+    });
+
+    map = JSON.parse(map.toString());
+
+    assert.equal(map.version, 3);
+    assert.equal(map.file, 'min.js');
+    assert.deepEqual(map.names, ['bar', 'baz', 'n']);
+    assert.deepEqual(map.sources, ['one.js', 'two.js']);
+    assert.equal(map.sourceRoot, '/the/root');
+    assert.equal(map.mappings, 'CACC,IAAI,IAAM,SAAUA,GAClB,OAAOC,IAAID;CCDb,IAAI,IAAM,SAAUE,GAClB,OAAOA');
   };
 
 });
