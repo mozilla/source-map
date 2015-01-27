3c89b7be288c7f34eb886a541376ee38e0579a65
Eddy Bruël <ejpbruel@mozilla.com>
2015-01-27 07:15:06 +0100

    Fuzzy search should find the closest element that is greater than or equal to.

--- test/source-map/test-binary-search.js
+++ test/source-map/test-binary-search.js
@@ -15,19 +15,8 @@ define(function (require, exports, module) {
     return a - b;
   }
 
-  exports['test too high'] = function (assert, util) {
+  exports['test too high with lub bias'] = function (assert, util) {
     var needle = 30;
-    var haystack = [2,4,6,8,10,12,14,16,18,20];
-
-    assert.doesNotThrow(function () {
-      binarySearch.search(needle, haystack, numberCompare);
-    });
-
-    assert.equal(haystack[binarySearch.search(needle, haystack, numberCompare)], 20);
-  };
-
-  exports['test too low'] = function (assert, util) {
-    var needle = 1;
     var haystack = [2,4,6,8,10,12,14,16,18,20];
 
     assert.doesNotThrow(function () {
@@ -37,18 +26,69 @@ define(function (require, exports, module) {
     assert.equal(binarySearch.search(needle, haystack, numberCompare), -1);
   };
 
-  exports['test exact search'] = function (assert, util) {
+  exports['test too low with lub bias'] = function (assert, util) {
+    var needle = 1;
+    var haystack = [2,4,6,8,10,12,14,16,18,20];
+
+    assert.doesNotThrow(function () {
+      binarySearch.search(needle, haystack, numberCompare, true);
+    });
+
+    assert.equal(haystack[binarySearch.search(needle, haystack, numberCompare)], 2);
+  };
+
+  exports['test exact search with lub bias'] = function (assert, util) {
     var needle = 4;
     var haystack = [2,4,6,8,10,12,14,16,18,20];
 
     assert.equal(haystack[binarySearch.search(needle, haystack, numberCompare)], 4);
   };
 
-  exports['test fuzzy search'] = function (assert, util) {
+  exports['test fuzzy search with lub bias'] = function (assert, util) {
     var needle = 19;
     var haystack = [2,4,6,8,10,12,14,16,18,20];
 
-    assert.equal(haystack[binarySearch.search(needle, haystack, numberCompare)], 18);
+    assert.equal(haystack[binarySearch.search(needle, haystack, numberCompare)], 20);
   };
 
+  exports['test too high with glb bias'] = function (assert, util) {
+    var needle = 30;
+    var haystack = [2,4,6,8,10,12,14,16,18,20];
+
+    assert.doesNotThrow(function () {
+      binarySearch.search(needle, haystack, numberCompare);
+    });
+
+    assert.equal(haystack[binarySearch.search(needle, haystack, numberCompare,
+                                              binarySearch.GREATEST_LOWER_BOUND)], 20);
+  };
+
+  exports['test too low with glb bias'] = function (assert, util) {
+    var needle = 1;
+    var haystack = [2,4,6,8,10,12,14,16,18,20];
+
+    assert.doesNotThrow(function () {
+      binarySearch.search(needle, haystack, numberCompare,
+                          binarySearch.GREATEST_LOWER_BOUND);
+    });
+
+    assert.equal(binarySearch.search(needle, haystack, numberCompare,
+                                     binarySearch.GREATEST_LOWER_BOUND), -1);
+  };
+
+  exports['test exact search with glb bias'] = function (assert, util) {
+    var needle = 4;
+    var haystack = [2,4,6,8,10,12,14,16,18,20];
+
+    assert.equal(haystack[binarySearch.search(needle, haystack, numberCompare,
+                          binarySearch.GREATEST_LOWER_BOUND)], 4);
+  };
+
+  exports['test fuzzy search with glb bias'] = function (assert, util) {
+    var needle = 19;
+    var haystack = [2,4,6,8,10,12,14,16,18,20];
+
+    assert.equal(haystack[binarySearch.search(needle, haystack, numberCompare,
+                          binarySearch.GREATEST_LOWER_BOUND)], 18);
+  };
 });
--- test/source-map/test-dog-fooding.js
+++ test/source-map/test-dog-fooding.js
@@ -60,25 +60,25 @@ define(function (require, exports, module) {
     // Fuzzy
 
     // Generated to original
-    util.assertMapping(2, 0, null, null, null, null, smc, assert, true);
-    util.assertMapping(2, 9, '/wu/tang/gza.coffee', 1, 0, null, smc, assert, true);
-    util.assertMapping(3, 0, null, null, null, null, smc, assert, true);
-    util.assertMapping(3, 9, '/wu/tang/gza.coffee', 2, 0, null, smc, assert, true);
-    util.assertMapping(4, 0, null, null, null, null, smc, assert, true);
-    util.assertMapping(4, 9, '/wu/tang/gza.coffee', 3, 0, null, smc, assert, true);
-    util.assertMapping(5, 0, null, null, null, null, smc, assert, true);
-    util.assertMapping(5, 9, '/wu/tang/gza.coffee', 4, 0, null, smc, assert, true);
-    util.assertMapping(6, 0, null, null, null, null, smc, assert, true);
-    util.assertMapping(6, 9, null, null, null, null, smc, assert, true);
-    util.assertMapping(6, 13, '/wu/tang/gza.coffee', 5, 10, null, smc, assert, true);
+    util.assertMapping(2, 0, '/wu/tang/gza.coffee', 1, 0, null, smc, assert, true);
+    util.assertMapping(2, 9, null, null, null, null, smc, assert, true);
+    util.assertMapping(3, 0, '/wu/tang/gza.coffee', 2, 0, null, smc, assert, true);
+    util.assertMapping(3, 9, null, null, null, null, smc, assert, true);
+    util.assertMapping(4, 0, '/wu/tang/gza.coffee', 3, 0, null, smc, assert, true);
+    util.assertMapping(4, 9, null, null, null, null, smc, assert, true);
+    util.assertMapping(5, 0, '/wu/tang/gza.coffee', 4, 0, null, smc, assert, true);
+    util.assertMapping(5, 9, null, null, null, null, smc, assert, true);
+    util.assertMapping(6, 0, '/wu/tang/gza.coffee', 5, 10, null, smc, assert, true);
+    util.assertMapping(6, 9, '/wu/tang/gza.coffee', 5, 10, null, smc, assert, true);
+    util.assertMapping(6, 13, null, null, null, null, smc, assert, true);
 
     // Original to generated
-    util.assertMapping(2, 2, '/wu/tang/gza.coffee', 1, 1, null, smc, assert, null, true);
-    util.assertMapping(3, 2, '/wu/tang/gza.coffee', 2, 3, null, smc, assert, null, true);
-    util.assertMapping(4, 2, '/wu/tang/gza.coffee', 3, 6, null, smc, assert, null, true);
-    util.assertMapping(5, 2, '/wu/tang/gza.coffee', 4, 9, null, smc, assert, null, true);
-    util.assertMapping(5, 2, '/wu/tang/gza.coffee', 5, 9, null, smc, assert, null, true);
-    util.assertMapping(6, 12, '/wu/tang/gza.coffee', 6, 19, null, smc, assert, null, true);
+    util.assertMapping(3, 2, '/wu/tang/gza.coffee', 1, 1, null, smc, assert, null, true);
+    util.assertMapping(4, 2, '/wu/tang/gza.coffee', 2, 3, null, smc, assert, null, true);
+    util.assertMapping(5, 2, '/wu/tang/gza.coffee', 3, 6, null, smc, assert, null, true);
+    util.assertMapping(6, 12, '/wu/tang/gza.coffee', 4, 9, null, smc, assert, null, true);
+    util.assertMapping(6, 12, '/wu/tang/gza.coffee', 5, 9, null, smc, assert, null, true);
+    util.assertMapping(null, null, '/wu/tang/gza.coffee', 6, 19, null, smc, assert, null, true);
   };
 
 });
