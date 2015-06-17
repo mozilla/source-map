bc84adb8cb5213a75f710edd2c2a668501ba6179
Nick Fitzgerald <fitzgen@gmail.com>
2015-06-17 11:48:04 -0700

    Optimize the base64.decode function.
    This function is incredibly hot while parsing mappings. By avoiding property
    gets and throwing errors in this function, we get about 2000 millisecond
    improvement on the mean time to parse our source map in bench.html.

--- test/source-map/test-base64.js
+++ test/source-map/test-base64.js
@@ -21,14 +21,12 @@ define(function (require, exports, module) {
   };
 
   exports['test out of range decoding'] = function (assert, util) {
-    assert.throws(function () {
-      base64.decode('=');
-    });
+    assert.equal(base64.decode('='.charCodeAt(0)), -1);
   };
 
   exports['test normal encoding and decoding'] = function (assert, util) {
     for (var i = 0; i < 64; i++) {
-      assert.equal(base64.decode(base64.encode(i)), i);
+      assert.equal(base64.decode(base64.encode(i).charCodeAt(0)), i);
     }
   };
 
