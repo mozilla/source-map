516f63b2ca3b24514b6c41443871f1a81ee6da17
Andrew Shaffer <andrew@weo.io>
2016-04-20 18:02:34 -0700

    fix style issues; check string manually for more fastness

--- dist/source-map.js
+++ dist/source-map.js
@@ -930,16 +930,55 @@ return /******/ (function(modules) { // webpackBootstrap
 	   *
 	   * @param String aStr
 	   */
+
 	  function toSetString(aStr) {
-	    if (/^\$*__proto__$/.test(aStr)) return '$' + aStr;
+	    if (isProtoString(aStr)) {
+	      return '$' + aStr;
+	    }
+
 	    return aStr;
 	  }
 	  exports.toSetString = toSetString;
 	   function fromSetString(aStr) {
-	    if (/^\$*__proto__$/.test(aStr)) return aStr.substr(1);
+	    if (isProtoString(aStr)) {
+	      return aStr.substr(1);
+	    }
+
 	    return aStr;
 	  }
 	  exports.fromSetString = fromSetString;
+
+	  function isProtoString(s) {
+	    if (!s) {
+	      return false;
+	    }
+
+	    var length = s.length;
+
+	    if (length < 9 /* "__proto__".length */) {
+	      return false;
+	    }
+
+	    if (s.charCodeAt(length - 1) !== 95  /* '_' */ ||
+	        s.charCodeAt(length - 2) !== 95  /* '_' */ ||
+	        s.charCodeAt(length - 3) !== 111 /* 'o' */ ||
+	        s.charCodeAt(length - 4) !== 116 /* 't' */ ||
+	        s.charCodeAt(length - 5) !== 111 /* 'o' */ ||
+	        s.charCodeAt(length - 6) !== 114 /* 'r' */ ||
+	        s.charCodeAt(length - 7) !== 112 /* 'p' */ ||
+	        s.charCodeAt(length - 8) !== 95  /* '_' */ ||
+	        s.charCodeAt(length - 9) !== 95  /* '_' */) {
+	      return false;
+	    }
+
+	    for (var i = length - 10; i >= 0; i--) {
+	      if (s.charCodeAt(i) !== 36 /* '$' */) {
+	        return false;
+	      }
+	    }
+
+	    return true;
+	  }
 
 	  /**
 	   * Comparator between two mappings where the original positions are compared.
