8647951386ac8671268985c21933d70e1b0b45e5
Nick Fitzgerald <fitzgen@gmail.com>
2018-01-19 12:39:05 -0800

    Remove the custom JS Quicksort implementation
    We originally added it because Array.prototype.sort was not self-hosted with
    custom comparison functions, but that's no longer true these days. We also do
    most of our heavy sorting in wasm now.

--- dist/source-map.js
+++ dist/source-map.js
@@ -7,7 +7,7 @@
 		exports["sourceMap"] = factory(require("fs"), require("path"));
 	else
 		root["sourceMap"] = factory(root["fs"], root["path"]);
-})(this, function(__WEBPACK_EXTERNAL_MODULE_11__, __WEBPACK_EXTERNAL_MODULE_12__) {
+})(this, function(__WEBPACK_EXTERNAL_MODULE_10__, __WEBPACK_EXTERNAL_MODULE_11__) {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -61,7 +61,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	 */
 	exports.SourceMapGenerator = __webpack_require__(1).SourceMapGenerator;
 	exports.SourceMapConsumer = __webpack_require__(7).SourceMapConsumer;
-	exports.SourceNode = __webpack_require__(14).SourceNode;
+	exports.SourceNode = __webpack_require__(13).SourceNode;
 
 
 /***/ }),
@@ -1413,9 +1413,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	var binarySearch = __webpack_require__(8);
 	var ArraySet = __webpack_require__(5).ArraySet;
 	var base64VLQ = __webpack_require__(2);
-	var quickSort = __webpack_require__(9).quickSort;
-	var readWasm = __webpack_require__(10);
-	var wasm = __webpack_require__(13);
+	var readWasm = __webpack_require__(9);
+	var wasm = __webpack_require__(12);
 
 	function SourceMapConsumer(aSourceMap, aSourceMapURL) {
 	  var sourceMap = aSourceMap;
@@ -2314,14 +2313,14 @@ return /******/ (function(modules) { // webpackBootstrap
 	IndexedSourceMapConsumer.prototype._sortGeneratedMappings =
 	  function IndexedSourceMapConsumer_sortGeneratedMappings() {
 	    const mappings = this._generatedMappingsUnsorted;
-	    quickSort(mappings, util.compareByGeneratedPositionsDeflated);
+	    mappings.sort(util.compareByGeneratedPositionsDeflated);
 	    this.__generatedMappings = mappings;
 	  };
 
 	IndexedSourceMapConsumer.prototype._sortOriginalMappings =
 	  function IndexedSourceMapConsumer_sortOriginalMappings() {
 	    const mappings = this._originalMappingsUnsorted;
-	    quickSort(mappings, util.compareByOriginalPositions);
+	    mappings.sort(util.compareByOriginalPositions);
 	    this.__originalMappings = mappings;
 	  };
 
@@ -2814,126 +2813,6 @@ return /******/ (function(modules) { // webpackBootstrap
 
 /***/ }),
 /* 9 */
-/***/ (function(module, exports) {
-
-	/* -*- Mode: js; js-indent-level: 2; -*- */
-	/*
-	 * Copyright 2011 Mozilla Foundation and contributors
-	 * Licensed under the New BSD license. See LICENSE or:
-	 * http://opensource.org/licenses/BSD-3-Clause
-	 */
-
-	// It turns out that some (most?) JavaScript engines don't self-host
-	// `Array.prototype.sort`. This makes sense because C++ will likely remain
-	// faster than JS when doing raw CPU-intensive sorting. However, when using a
-	// custom comparator function, calling back and forth between the VM's C++ and
-	// JIT'd JS is rather slow *and* loses JIT type information, resulting in
-	// worse generated code for the comparator function than would be optimal. In
-	// fact, when sorting with a comparator, these costs outweigh the benefits of
-	// sorting in C++. By using our own JS-implemented Quick Sort (below), we get
-	// a ~3500ms mean speed-up in `bench/bench.html`.
-
-	/**
-	 * Swap the elements indexed by `x` and `y` in the array `ary`.
-	 *
-	 * @param {Array} ary
-	 *        The array.
-	 * @param {Number} x
-	 *        The index of the first item.
-	 * @param {Number} y
-	 *        The index of the second item.
-	 */
-	function swap(ary, x, y) {
-	  var temp = ary[x];
-	  ary[x] = ary[y];
-	  ary[y] = temp;
-	}
-
-	/**
-	 * Returns a random integer within the range `low .. high` inclusive.
-	 *
-	 * @param {Number} low
-	 *        The lower bound on the range.
-	 * @param {Number} high
-	 *        The upper bound on the range.
-	 */
-	function randomIntInRange(low, high) {
-	  return Math.round(low + (Math.random() * (high - low)));
-	}
-
-	/**
-	 * The Quick Sort algorithm.
-	 *
-	 * @param {Array} ary
-	 *        An array to sort.
-	 * @param {function} comparator
-	 *        Function to use to compare two items.
-	 * @param {Number} p
-	 *        Start index of the array
-	 * @param {Number} r
-	 *        End index of the array
-	 */
-	function doQuickSort(ary, comparator, p, r) {
-	  // If our lower bound is less than our upper bound, we (1) partition the
-	  // array into two pieces and (2) recurse on each half. If it is not, this is
-	  // the empty array and our base case.
-
-	  if (p < r) {
-	    // (1) Partitioning.
-	    //
-	    // The partitioning chooses a pivot between `p` and `r` and moves all
-	    // elements that are less than or equal to the pivot to the before it, and
-	    // all the elements that are greater than it after it. The effect is that
-	    // once partition is done, the pivot is in the exact place it will be when
-	    // the array is put in sorted order, and it will not need to be moved
-	    // again. This runs in O(n) time.
-
-	    // Always choose a random pivot so that an input array which is reverse
-	    // sorted does not cause O(n^2) running time.
-	    var pivotIndex = randomIntInRange(p, r);
-	    var i = p - 1;
-
-	    swap(ary, pivotIndex, r);
-	    var pivot = ary[r];
-
-	    // Immediately after `j` is incremented in this loop, the following hold
-	    // true:
-	    //
-	    //   * Every element in `ary[p .. i]` is less than or equal to the pivot.
-	    //
-	    //   * Every element in `ary[i+1 .. j-1]` is greater than the pivot.
-	    for (var j = p; j < r; j++) {
-	      if (comparator(ary[j], pivot) <= 0) {
-	        i += 1;
-	        swap(ary, i, j);
-	      }
-	    }
-
-	    swap(ary, i + 1, j);
-	    var q = i + 1;
-
-	    // (2) Recurse on each half.
-
-	    doQuickSort(ary, comparator, p, q - 1);
-	    doQuickSort(ary, comparator, q + 1, r);
-	  }
-	}
-
-	/**
-	 * Sort the given array in-place with the given comparator function.
-	 *
-	 * @param {Array} ary
-	 *        An array to sort.
-	 * @param {function} comparator
-	 *        Function to use to compare two items.
-	 */
-	exports.quickSort = function (ary, comparator) {
-	  doQuickSort(ary, comparator, 0, ary.length - 1);
-	};
-
-
-/***/ }),
-/* 10 */
 /***/ (function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(__dirname) {if (typeof fetch === "function") {
@@ -2955,8 +2834,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	  module.exports.initialize = url => mappingsWasmUrl = url;
 	} else {
 	  // Node version of reading a wasm file into an array buffer.
-	  const fs = __webpack_require__(11);
-	  const path = __webpack_require__(12);
+	  const fs = __webpack_require__(10);
+	  const path = __webpack_require__(11);
 
 	  module.exports = function readWasm() {
 	    return new Promise((resolve, reject) => {
@@ -2980,6 +2859,12 @@ return /******/ (function(modules) { // webpackBootstrap
 	/* WEBPACK VAR INJECTION */}.call(exports, "/"))
 
 /***/ }),
+/* 10 */
+/***/ (function(module, exports) {
+
+	module.exports = __WEBPACK_EXTERNAL_MODULE_10__;
+
+/***/ }),
 /* 11 */
 /***/ (function(module, exports) {
 
@@ -2987,15 +2872,9 @@ return /******/ (function(modules) { // webpackBootstrap
 
 /***/ }),
 /* 12 */
-/***/ (function(module, exports) {
-
-	module.exports = __WEBPACK_EXTERNAL_MODULE_12__;
-
-/***/ }),
-/* 13 */
 /***/ (function(module, exports, __webpack_require__) {
 
-	const readWasm = __webpack_require__(10);
+	const readWasm = __webpack_require__(9);
 
 	/**
 	 * Provide the JIT with a nice shape / hidden class.
@@ -3105,7 +2984,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ }),
-/* 14 */
+/* 13 */
 /***/ (function(module, exports, __webpack_require__) {
 
 	/* -*- Mode: js; js-indent-level: 2; -*- */
