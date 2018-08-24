13e1439efb71b619e495546bf3b28bc6e8d00ed4
Cory Snider <cory.snider@fitbit.com>
2018-08-24 18:50:54 -0400

    Inline wasm module into the browser build
    Make the existence of the wasm module as much of an implementation
    detail in the browser as it is in Node.js by inlining its contents into
    the browser build. It is no longer necessary to call
    `SourceMapConsumer.initialize()` when using it in a browser environment.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const webpack = require("webpack");
+
 const distDir = path.join(__dirname, "dist");
 
 module.exports = [
@@ -11,10 +13,31 @@ module.exports = [
       filename: "source-map.js",
       library: "sourceMap",
       libraryTarget: "umd",
+      // Needed for the build to work in both browser and webworker
+      // until webpack gains a "universal" target.
+      globalObject: "this",
     },
     externals: [
       "fs",
       "path",
-    ]
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.wasm$/,
+          type: "javascript/auto",
+          // arraybuffer-loader would be much more convenient, but it
+          // pulls an unnecessary Node.js Buffer polyfill into the
+          // bundle.
+          use: "base64-loader",
+        },
+      ],
+    },
+    plugins: [
+      new webpack.NormalModuleReplacementPlugin(
+        /lib\/read-wasm\.js$/,
+        "./read-wasm-browser.js",
+      ),
+    ],
   }
 ];
